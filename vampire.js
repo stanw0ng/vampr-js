9894d40436e15bfcbb5bf0f75a91e7f1bb89d6bc
stanw0ng <user.stanley@gmail.com>
2023-03-26 14:41:29 -0400

    updated vampire.js to pass all tests

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numOfVampires ++;
+    }
+
+    return numOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      return false;
+    }
 
+    return true;
   }
 
   /** Stretch **/
@@ -36,7 +49,27 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let thisVampNum = this.numberOfVampiresFromOriginal;
+    let otherVampNum = vampire.numberOfVampiresFromOriginal;
+    let thisVamp = this;
+    let otherVamp = vampire;
 
+    while (thisVampNum > otherVampNum) { // moves "up" (by decreasing number) and making current Vampire its creator until same level is reached
+      thisVamp = thisVamp.creator;
+      thisVampNum --;
+    }
+
+    while (thisVampNum < otherVampNum) { // moves "up" (by decreasing number) and making other Vampire its creator until same level is reached
+      otherVamp = otherVamp.creator;
+      otherVampNum --;
+    }
+
+    while (thisVamp !== otherVamp) { // once numbers are the same, each vampire becomes their creators until the names are the same
+      thisVamp = thisVamp.creator;
+      otherVamp = otherVamp.creator;
+    }
+
+    return thisVamp; // once they are the same, return either Vamp
   }
 }
 
